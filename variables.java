273496fc32789bba70409cb61749108bc3c4864c
bekirglr <bbekirglr@gmail.com>
2022-09-15 17:25:12 +0300

    variables

---
+++ variables.java
@@ -0,0 +1,18 @@
+public class variables {
+    public static void main(String[] args) {
+
+        int ogrsayısı = 11; //int tam sayı değişkenlerinde kullanılır
+        System.out.println("öğrenci sayısı:"+ ogrsayısı);
+
+        String mesaj = "Öğrenci Sayısı"; //string ifadesini metinsel karekterlerde kullanıyoruz
+        System.out.println(mesaj + ogrsayısı);
+
+        /*Bunun gibi birkaç variables(değişken) daha var. for exp:
+
+        float - 19.99 veya -19,99 gibi ondalık sayılarla kayan noktalı sayıları saklar
+        char - 'a' veya 'B' gibi tek karakterleri saklar. Karakter değerleri tek tırnak içine alınır
+        boolean - stores values with two states: true or false
+
+        */
+    }
+}
