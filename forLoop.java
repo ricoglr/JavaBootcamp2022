1a8fa42627c0163cf4f7b70183b49480de561722
bekirglr <bbekirglr@gmail.com>
2022-09-15 17:51:33 +0300

    for loop

---
+++ forLoop.java
@@ -0,0 +1,16 @@
+public class forLoop {
+    public static void main(String[] args) {
+        /*
+         for (değişkeni tanımla; şartı yaz; süreci belirle) {
+            bu süreçte ne olacağını yaz
+         }
+         */
+        System.out.println("Döngü Başladı");
+
+        for (int i=0; i<=10; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("Döngü Bitti");
+    }
+}
