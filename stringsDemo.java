e4c76f2a6f5e871833a7cd34ba6e93cd8a20b79f
bekirglr <bbekirglr@gmail.com>
2022-09-16 00:35:53 +0300

    String ifadeler kullanıldı

---
+++ stringsDemo.java
@@ -0,0 +1,19 @@
+public class stringsDemo {
+
+    public static void main(String[] args) {
+        String mesaj = "Bugün java öğrenmeye başladım.";
+        System.out.println(mesaj);
+
+        System.out.println("Eleman sayısı: "+ mesaj.length());
+        System.out.println("5. Eleman: " + mesaj.charAt(4));
+        System.out.println(mesaj.concat("Ne Güzel :)"));
+        System.out.println(mesaj.startsWith("B"));
+        System.out.println(mesaj.endsWith("."));
+
+        char[] karakterler = new char[5];
+        mesaj.getChars(0, 5, karakterler, 0);
+        System.out.println(karakterler);
+        System.out.println(mesaj.indexOf("aş"));
+        System.out.println(mesaj.lastIndexOf("e"));
+    }
+}
