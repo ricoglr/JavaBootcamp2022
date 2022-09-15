a4e92e46e05bfb710167644d660e8b7bee741704
bekirglr <bbekirglr@gmail.com>
2022-09-15 17:31:24 +0300

    if-else

---
+++ conditionals.java
@@ -0,0 +1,15 @@
+public class conditionals {
+    public static void main(String[] args) {
+
+        int sayi = 20;
+        
+        if(sayi==20){
+            System.out.println("Sayı 20'ye eşittir");
+        }
+        if(sayi < 20){
+            System.out.println("Sayı 20'den küçüktür");
+        }else{
+            System.out.println("Sayı 20'den büyüktür");
+        }
+    }
+}
