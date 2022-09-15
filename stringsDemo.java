328c1e2280a08c69555da6d537d119dbb33ce147
bekirglr <bbekirglr@gmail.com>
2022-09-16 00:42:01 +0300

    Strings Demo 2

--- stringsDemo.java
+++ stringsDemo.java
@@ -4,16 +4,30 @@ public class stringsDemo {
         String mesaj = "Bugün java öğrenmeye başladım.";
         System.out.println(mesaj);
 
-        System.out.println("Eleman sayısı: "+ mesaj.length());
-        System.out.println("5. Eleman: " + mesaj.charAt(4));
-        System.out.println(mesaj.concat("Ne Güzel :)"));
-        System.out.println(mesaj.startsWith("B"));
-        System.out.println(mesaj.endsWith("."));
+        // System.out.println("Eleman sayısı: "+ mesaj.length());
+        // System.out.println("5. Eleman: " + mesaj.charAt(4));
+        // System.out.println(mesaj.concat("Ne Güzel :)"));
+        // System.out.println(mesaj.startsWith("B"));
+        // System.out.println(mesaj.endsWith("."));
 
-        char[] karakterler = new char[5];
-        mesaj.getChars(0, 5, karakterler, 0);
-        System.out.println(karakterler);
-        System.out.println(mesaj.indexOf("aş"));
-        System.out.println(mesaj.lastIndexOf("e"));
+        // char[] karakterler = new char[5];
+        // mesaj.getChars(0, 5, karakterler, 0);
+        // System.out.println(karakterler);
+        // System.out.println(mesaj.indexOf("aş"));
+        // System.out.println(mesaj.lastIndexOf("e"));
+
+        /* ******************************************************************************* */
+
+        String yeniMesaj = mesaj.replace(' ', '-');
+        System.out.println(yeniMesaj);
+        System.out.println(mesaj.substring(2, 5));
+
+        for(String kelime: mesaj.split(" ")){
+            System.out.println(kelime);
+        }
+
+        System.out.println(mesaj.toLowerCase());
+        System.out.println(mesaj.toUpperCase());
+        System.out.println(mesaj.trim());
     }
 }
