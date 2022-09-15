2b8cce51943fc877d1e47afe5b48fea31e747285
bekirglr <bbekirglr@gmail.com>
2022-09-15 17:54:35 +0300

    while loop

---
+++ whileLoop.java
@@ -0,0 +1,18 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        
+        /*
+         döngü kurmadan önce değişkeni tanımla
+         while (şartı yaz){
+            fonksiyonu yaz.
+         }
+         */
+
+        int i = 1;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Döngü Bitti");
+    }
+}
