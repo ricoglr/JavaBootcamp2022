e4ccd4866931da688521b8e9825997c1fab9ea3b
bekirglr <bbekirglr@gmail.com>
2022-09-22 00:28:59 +0300

    private olan field ları get set ile yazdım

--- OOP1/IndividualCustomer.java
+++ OOP1/IndividualCustomer.java
@@ -3,5 +3,21 @@ package OOP1;
 public class IndividualCustomer extends Customer{
     private String firstName;
     private String lastName;
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
     
 }
