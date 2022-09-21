e5899df9ab5ef445036e8816fe9d4614b27ae5cb
bekirglr <bbekirglr@gmail.com>
2022-09-22 00:12:08 +0300

    field ları privat yapıp get set uyguladım

--- OOP1/Product.java
+++ OOP1/Product.java
@@ -1,9 +1,51 @@
 package OOP1;
 
 public class Product {
-    String name;
-    double price;
-    double discount;
-    int stock;
-    String imgUrl;
+    private String name;
+    private double price;
+    private double discount;
+    private int stock;
+    private String imgUrl;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return this.discount;
+    }
+
+    public void setDiscount(double discount) {
+        this.discount = discount;
+    }
+
+    public int getStock() {
+        return this.stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+
 }
