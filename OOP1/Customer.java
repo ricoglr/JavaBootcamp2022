25205cc99484d4356a1694024bec71616d9da35e
bekirglr <bbekirglr@gmail.com>
2022-09-22 00:11:43 +0300

    get ve set metotlarını yazdım

--- OOP1/Customer.java
+++ OOP1/Customer.java
@@ -3,5 +3,30 @@ package OOP1;
 public class Customer {
     private int id;
     private String customerNumber;
-    private String phone;     
+    private String phone;
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerNumber() {
+        return this.customerNumber;
+    }
+
+    public void setCustomerNumber(String customerNumber) {
+        this.customerNumber = customerNumber;
+    }
+
+    public String getPhone() {
+        return this.phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
 }
