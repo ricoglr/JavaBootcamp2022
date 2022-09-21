e7404a44bf824e934a47d02774a4a69cdea320c9
bekirglr <bbekirglr@gmail.com>
2022-09-21 22:58:01 +0300

    nesneleri yazdırdım

--- OOP1/Main.java
+++ OOP1/Main.java
@@ -3,6 +3,27 @@ package OOP1;
 public class Main {
 
     public static void main(String[] args) {
+
+        Product product1 = new Product();
+        product1.name = "Macbook Pro";
+        product1.price = 27.900;
+        product1.stock = 3;
+        product1.discount = 7;
+        product1.imgUrl = "demo.jpg";
+
+        Product product2 = new Product();
+        product1.name = "Macbook Pro";
+        product1.price = 16.900;
+        product1.stock = 7;
+        product1.discount = 2;
+        product1.imgUrl = "demo2.jpg";
+
+        Product product3 = new Product();
+        product1.name = "Macbook Pro";
+        product1.price = 20.100;
+        product1.stock = 9;
+        product1.discount = 2;
+        product1.imgUrl = "dem3.jpg";
         
     }
 }
