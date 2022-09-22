ba9561ef62ab5836cdf1c4d03e94e79c5a21e6e7
bekirglr <bbekirglr@gmail.com>
2022-09-22 18:09:44 +0300

    referansları anladım

--- Hafta2_Odev2/Classes/Main.java
+++ Hafta2_Odev2/Classes/Main.java
@@ -11,7 +11,19 @@ public class Main {
         customerManager.Remove();
         customerManager.Update();
 
+        //Value
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi2 = sayi1;
+        sayi1=30;
+        System.out.println(sayi2);
 
+        int[] numbers1 = new int[]{1,2,3};
+        int[] numbers2 = new int[]{4,5,6};
+
+        numbers2 = numbers1;
+        numbers1[0]=10;
+        System.out.println(numbers2[0]);
 
         System.out.println("--------------------------------");
     }
