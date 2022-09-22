ebe8577524bd30a0887d149bb3605d76da6e6d8f
bekirglr <bbekirglr@gmail.com>
2022-09-22 18:22:42 +0300

    Basit Bir Hesap Makinesi yapıldı

---
+++ Hafta2_Odev2/ReCapDemo_Classes/DortIslem.java
@@ -0,0 +1,20 @@
+package Hafta2_Odev2.ReCapDemo_Classes;
+
+public class DortIslem {
+
+    public int Topla(int num1, int num2){
+        return num1 + num2;
+    }
+
+    public int Cikar(int num1, int num2){
+        return num1 - num2;
+    }
+
+    public int Carp(int num1, int num2){
+        return num1 * num2;
+    }
+
+    public int Bol(int num1, int num2){
+        return num1 / num2;
+    }
+}
--- Hafta2_Odev2/ReCapDemo_Classes/Main.java
+++ Hafta2_Odev2/ReCapDemo_Classes/Main.java
@@ -2,6 +2,9 @@ package Hafta2_Odev2.ReCapDemo_Classes;
 
 public class Main {
     public static void main(String[] args) {
+        DortIslem dortIslem = new DortIslem();
         
+        int sonuc = dortIslem.Carp(2, 5);
+        System.out.println(sonuc); 
     }
 }
