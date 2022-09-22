ba4afab255297d140b194abc2aafd8830bfea142
bekirglr <bbekirglr@gmail.com>
2022-09-22 14:28:25 +0300

    Method yapısını öğrendim

--- Hafta2_Odev2/Methods/Main.java
+++ Hafta2_Odev2/Methods/Main.java
@@ -1,7 +1,36 @@
 package Hafta2_Odev2.Methods;
 
 public class Main {
+    
     public static void main(String[] args) {
         
+        sayiBulmaca();
+    }
+
+    public static void sayiBulmaca(){
+
+        int sayilar[] = new int []{1,2,5,7,9};
+        int aranacak = 6;
+        boolean varMi = false;
+
+        for(int sayi : sayilar){
+            if (sayi == aranacak) {
+                varMi = true;
+                break;
+            }
+        }
+
+        String mesaj = "";
+        if (varMi) {
+            mesajVer("Sayı Mevcuttur: " + aranacak);
+            mesajVer(mesaj);
+        }
+        else{
+            mesajVer("Sayı Mevcut Değildir: " + aranacak);
+        }
+    }
+
+    public static void mesajVer (String mesaj){
+        System.out.println(mesaj);
     }
 }
