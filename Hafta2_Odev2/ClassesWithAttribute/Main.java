a82d99df17d919254057114ebb9ef7da5ae0f0ab
bekirglr <bbekirglr@gmail.com>
2022-09-22 18:51:06 +0300

    Product Manager oluşturuldu

--- Hafta2_Odev2/ClassesWithAttribute/Main.java
+++ Hafta2_Odev2/ClassesWithAttribute/Main.java
@@ -5,19 +5,25 @@ public class Main {
         System.out.println("--------------------------------------");
 
 
+
         Product product = new Product();
+
         product.id = 0;
         product.name = "MacBook Pro";
         product.description = "Laptop";
         product.price = 27.100;
         product.stock= 5;
+
+        /*
         System.out.println("Product ID ---> " + product.id);
         System.out.println("Product Name ---> " + product.name);
         System.out.println("Product Description ---> " + product.description);
         System.out.println("Product Price ---> " + product.price);
         System.out.println("Product Stock ---> " + product.stock);
+        */
 
-
+        ProductManage productManage = new ProductManage();
+        productManage.Add(product);
 
 
 
--- Hafta2_Odev2/ClassesWithAttribute/ProductManage.java
+++ Hafta2_Odev2/ClassesWithAttribute/ProductManage.java
@@ -1,5 +1,9 @@
 package Hafta2_Odev2.ClassesWithAttribute;
 
 public class ProductManage {
-    
+    public void Add(Product product){
+        
+        //JDBC
+        System.out.println("Added Product ---> " + product.name);
+    }
 }
