f98d15508a10d30a9969617d753c9edde066888f
bekirglr <bbekirglr@gmail.com>
2022-09-22 14:49:39 +0300

    Void yapısını öğrendim ve parametreli methodları pekiştirdim

--- Hafta2_Odev2/Methods2/Main.java
+++ Hafta2_Odev2/Methods2/Main.java
@@ -1,7 +1,35 @@
 package Hafta2_Odev2.Methods2;
 
+import java.security.PublicKey;
+
 public class Main {
     public static void main(String[] args) {
+        String mesaj= "Bugün hava çok güzel.";
+        String yeniMesaj= sehirVer();
+        System.out.println(yeniMesaj);
+        int sayi = topla(5,7);
+        System.out.println(sayi);
+    }
+    
+    public static void ekle(){
+        System.out.println("EKlendi");
+    }
+
+    public static void sil(){
+        System.out.println("silindi");
+    }
+
+    public static void güncelle(){
+        System.out.println("Güncellendi");
+        return;
+    }
+
+    public static int topla(int x, int y) {
         
+        return x+y;
+    }
+
+    public static String sehirVer() {
+        return "Hatay";
     }
 }
