a9f75c0ee67120a085ad4e61592976b9b17ef47b
bekirglr <bbekirglr@gmail.com>
2022-09-22 18:02:28 +0300

    customerManager classını main de çağırdım

--- Hafta2_Odev2/Classes/Main.java
+++ Hafta2_Odev2/Classes/Main.java
@@ -2,6 +2,17 @@ package Hafta2_Odev2.Classes;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("--------------------------------");
+
+        //reference type
+        CustomerManager customerManager = new CustomerManager();
         
+        customerManager.Add();
+        customerManager.Remove();
+        customerManager.Update();
+
+
+
+        System.out.println("--------------------------------");
     }
 }
