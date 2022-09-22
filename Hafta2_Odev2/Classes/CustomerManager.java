b8838e9511228194dac80d3855d337aac6ada0a4
bekirglr <bbekirglr@gmail.com>
2022-09-22 15:46:48 +0300

    Class yapısını anladım

--- Hafta2_Odev2/Classes/CustomerManager.java
+++ Hafta2_Odev2/Classes/CustomerManager.java
@@ -1,5 +1,17 @@
 package Hafta2_Odev2.Classes;
 
+import java.security.PublicKey;
+
 public class CustomerManager {
-    
+    public void Add(){
+        System.out.println("Müşteri EKlendi");
+    }
+
+    public void Remove(){
+        System.out.println("Müşteri Silindi");
+    }
+
+    public void Update(){
+        System.out.println("Müşteri Güncellendi");
+    }
 }
