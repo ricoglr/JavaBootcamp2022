4d8816f14bd62bc85cf5ab0623a92b6fdecc2511
bekirglr <bbekirglr@gmail.com>
2022-09-22 18:45:18 +0300

    Product nesnemi oluşturdum

---
+++ Hafta2_Odev2/ClassesWithAttribute/Main.java
@@ -0,0 +1,27 @@
+package Hafta2_Odev2.ClassesWithAttribute;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------");
+
+
+        Product product = new Product();
+        product.id = 0;
+        product.name = "MacBook Pro";
+        product.description = "Laptop";
+        product.price = 27.100;
+        product.stock= 5;
+        System.out.println("Product ID ---> " + product.id);
+        System.out.println("Product Name ---> " + product.name);
+        System.out.println("Product Description ---> " + product.description);
+        System.out.println("Product Price ---> " + product.price);
+        System.out.println("Product Stock ---> " + product.stock);
+
+
+
+
+
+
+        System.out.println("--------------------------------------");
+    }
+}
---
+++ Hafta2_Odev2/ClassesWithAttribute/Product.java
@@ -0,0 +1,10 @@
+package Hafta2_Odev2.ClassesWithAttribute;
+
+public class Product {
+    //Attribute | Field
+    int id;
+    String name;
+    String description;
+    double price;
+    int stock;
+}
