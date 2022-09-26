f35f2dc565a021552c38da2285379d1662584ac8
bekirglr <bbekirglr@gmail.com>
2022-09-26 20:30:04 +0300

    main metodu oluşturuldu

--- Hafta2_Odev2/Constructor/Main.java
+++ Hafta2_Odev2/Constructor/Main.java
@@ -6,26 +6,11 @@ public class Main {
     
     
     
-        Product product = new Product();
-
-        product.setId(0);
-        product.setName("MacBook Pro");
-        product.setDescription("Laptop");
-        product.setPrice(27.100);
-        product.setStock(5);
-
-        /*
-        System.out.println("Product ID ---> " + product.id);
-        System.out.println("Product Name ---> " + product.name);
-        System.out.println("Product Description ---> " + product.description);
-        System.out.println("Product Price ---> " + product.price);
-        System.out.println("Product Stock ---> " + product.stock);
-        */
-
+        Product product = new Product(1,"MacBook","Laptop",5,"Uzay Grisi");
+        
         ProductManager productManager = new ProductManager();
+
         productManager.Add(product);
-
-
 
         System.out.println("--------------------------------------");
     }
